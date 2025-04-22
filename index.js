2ec93cc528c8ef2251879d2af4d6d2960d7e48ad
bilalfoyok <budidanfauzan@gmail.com>
2025-04-22 17:09:34 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Andra porn</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="container">
+        <h1>bilal dendang</h1>
+        <img src="./poyok.jpg">
+        <p>bilal goyang goyang, mantap. cihuyy. bilal ini suka lelaki. ini video bukti bahwa kalau bilal suka lelaki.</p>
+
+        <button id="youtubeButton" class="BS">Video panas Andra limited edition</button>
+    </div>
+
+    <!-- Pastikan script.js dihubungkan di bagian bawah agar HTML sudah dimuat -->
+    <script src="js.js"></script>
+</body>
+</html>
